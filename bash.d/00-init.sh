0dfa4f4a770550d755822e548a7d5a964de6e3a1
Vasili Sviridov <vasili@sviridov.ca>
2019-12-08 18:24:20 -0800

    finalizer

--- bash.d/00-init.sh
+++ bash.d/00-init.sh
@@ -4,3 +4,8 @@
 DOTFILES=$( cd "$(dirname "${BASH_SOURCE[0]}")"/.. && pwd || return)
 
 SUBMODULES=$( cd "$DOTFILES/submodules" && pwd)
+
+function finalize {
+    unset DOTFILES
+    unset SUBMODULES
+}
